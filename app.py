c7aab7985803eb37362a662b8d0138e8eca4b7b3
Michal Migurski <mike@teczno.com>
2015-08-14 16:40:33 -0700

    Responding to a number

--- app.py
+++ app.py
@@ -13,8 +13,10 @@ def call():
     print '/call:', repr(request.form)
     return '''<?xml version="1.0" encoding="UTF-8"?>
 <Response>
-<Gather timeout="10" numDigits="1" action="/select">
-<Say voice="man" language="en">Speak friend and enter</Say>
+<Gather timeout="15" numDigits="1" action="/select">
+<Say voice="man" language="en">Press 1 for English.</Say>
+<Say voice="man" language="en">Press 2 for Spanish.</Say>
+<Say voice="man" language="en">Press 3 for Chinese.</Say>
 </Gather>
 </Response>'''
 
@@ -23,10 +25,11 @@ def select():
     print '/select:', request
     print '/select:', repr(request.data)
     print '/select:', repr(request.form)
+    digits = request.form.get('Digits')
     return '''<?xml version="1.0" encoding="UTF-8"?>
 <Response>
-<Say voice="man" language="en">Speak friend and enter</Say>
-</Response>'''
+<Say voice="man" language="en">You typed the number {}</Say>
+</Response>'''.format(digits)
 
 if __name__ == '__main__':
     app.run(debug=True)
