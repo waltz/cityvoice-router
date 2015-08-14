2666a9c34aa9a37aeee2b1ed468f08361746b51f
Michal Migurski <mike@teczno.com>
2015-08-14 16:31:27 -0700

    Stubbed in call method

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Hello world'
+
+@app.route('/call', methods=['POST'])
+def call():
+    print request
+    print repr(request.data)
+    print repr(request.form)
+    return 'Goodbye World'
 
 if __name__ == '__main__':
     app.run(debug=True)
