6273a0cc95b0202324cae07d32c57f4b2cf19ee8
Michal Migurski <mike@teczno.com>
2015-08-14 16:38:15 -0700

    Added selection button push

--- app.py
+++ app.py
@@ -8,12 +8,24 @@ def index():
 
 @app.route('/call', methods=['POST'])
 def call():
-    print request
-    print repr(request.data)
-    print repr(request.form)
+    print '/call:', request
+    print '/call:', repr(request.data)
+    print '/call:', repr(request.form)
     return '''<?xml version="1.0" encoding="UTF-8"?>
 <Response>
-    <Say voice="man" language="en">Speak friend and enter</Say>
+<Gather timeout="10" numDigits="1" action="/select">
+<Say voice="man" language="en">Speak friend and enter</Say>
+</Gather>
+</Response>'''
+
+@app.route('/select', methods=['POST'])
+def select():
+    print '/select:', request
+    print '/select:', repr(request.data)
+    print '/select:', repr(request.form)
+    return '''<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+<Say voice="man" language="en">Speak friend and enter</Say>
 </Response>'''
 
 if __name__ == '__main__':
